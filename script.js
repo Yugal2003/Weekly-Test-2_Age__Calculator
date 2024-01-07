720027eeb17b7fc7ab97a5d69248a41191b5e790
Yugal2003 <132428388+Yugal2003@users.noreply.github.com>
2024-01-07 14:47:28 +0530

    Age Calculator

--- script.js
+++ script.js
@@ -2,26 +2,26 @@ let selectDate   = document.querySelector("#date_input");
 let clickButton  = document.querySelector(".age_button");
 let showYear     = document.querySelector(".age_count");
 
-clickButton.addEventListener("click" , () =>{
+clickButton.addEventListener("click" , function(){
     if(selectDate.value === ""){
         alert("Please enter your birthday:")
     }
     else{
         //select time calculator
         let date = new Date(selectDate.value);
-        console.log(date);
+        // console.log(date);
         let date_year = date.getFullYear();
-        console.log(date_year);
+        // console.log(date_year);
 
         // now time calculator
         let currDate = new Date();
-        console.log(currDate);
+        // console.log(currDate);
         let currDate_year = currDate.getFullYear();
-        console.log(currDate_year);
+        // console.log(currDate_year);
 
         // find your age answer
         let yourAge = (currDate_year - date_year) - 1;
-        console.log(yourAge);
+        // console.log(yourAge);
 
         showYear.innerHTML = `Your age is ${yourAge} years old`;
     }
